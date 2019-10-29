dcef7d21e6c0c0ccb89f33955221e601c50f6203
hellobrian <thisisbrianhan@gmail.com>
2019-10-28 22:47:47 -0500

    Remove TestComp

--- packages/test-comp/src/TestComp.stories.js
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-import TestComp from "../build/index.esm";
-
-export default {
-  title: "TestComp"
-};
-
-export const Default = () => <TestComp>wut up</TestComp>;
--- packages/test-comp/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import { TestComp } from "./TestComp";
-export default TestComp
-
-
-
