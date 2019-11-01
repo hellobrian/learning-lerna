4e14ae6976092476320c3aa78273266a621ce40f
hellobrian <thisisbrianhan@gmail.com>
2019-11-01 11:11:15 -0500

    improvement(icon.stories.js): use icon-manifest for select knob
    automatically provide select options for name prop on Icon component supplied from
    icon-manifest.json

--- packages/icon/src/Icon.stories.js
+++ packages/icon/src/Icon.stories.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Icon from '../build/index.esm';
 import { select, text } from '@storybook/addon-knobs';
+import iconManifest from '../icon-manifest.json';
+
+console.log({ iconManifest });
 
 export default {
     title: 'Icon'
 };
 
-const options = ['calendar', 'chevron'];
+const options = iconManifest.icons.map((icon) => icon.name);
 
 export const Default = () => (
     <Icon
