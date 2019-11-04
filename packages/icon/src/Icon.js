6e57e82d18ac9dc928f32a86f67e26377423cd4d
hellobrian <thisisbrianhan@gmail.com>
2019-11-04 09:37:34 -0600

    feat(icon): add House Icon

--- packages/icon/src/Icon.js
+++ packages/icon/src/Icon.js
@@ -7,13 +7,15 @@ import Calendar from './svg/calendar.svg';
 import Chevron from './svg/chevron.svg';
 import Clock from './svg/clock.svg';
 import Close from './svg/close.svg';
+import House from './svg/house.svg';
 import Menu from './svg/menu.svg';
 import People from './svg/people.svg';
+import SignOut from './svg/sign-out.svg';
 
 // styles
 import styles from './Icon.module.scss';
 
-export function Icon({ name = '', className = '', ...props }) {
+export function Icon({ name = '', className = '', children, ...props }) {
     const classList = [styles.root, className].join(' ').trim();
     const iconProps = { className: classList, ...props };
     const formattedName = name.toLowerCase();
@@ -24,8 +26,11 @@ export function Icon({ name = '', className = '', ...props }) {
             {formattedName === 'chevron' && <Chevron {...iconProps} />}
             {formattedName === 'clock' && <Clock {...iconProps} />}
             {formattedName === 'close' && <Close {...iconProps} />}
+            {formattedName === 'house' && <House {...iconProps} />}
             {formattedName === 'menu' && <Menu {...iconProps} />}
             {formattedName === 'people' && <People {...iconProps} />}
+            {formattedName === 'sign-out' && <SignOut {...iconProps} />}
+            {children}
         </Fragment>
     );
 }
